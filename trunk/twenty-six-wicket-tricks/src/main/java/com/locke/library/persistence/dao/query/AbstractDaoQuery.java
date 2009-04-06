345f5fea542a99744dd72219d01f17b2261b32fd
jonathan.locke <jonathan.locke@bcb4f866-e984-11dd-bce9-bf72c0239d3a>
2009-04-06 20:40:27 +0000

    result iteration/paging

--- trunk/twenty-six-wicket-tricks/src/main/java/com/locke/library/persistence/dao/query/AbstractDaoQuery.java
+++ trunk/twenty-six-wicket-tricks/src/main/java/com/locke/library/persistence/dao/query/AbstractDaoQuery.java
@@ -1,31 +1,31 @@
 package com.locke.library.persistence.dao.query;
 
 import java.io.Serializable;
-import java.util.List;
+import java.util.Iterator;
 
 import com.locke.library.persistence.IPersistent;
 
 public abstract class AbstractDaoQuery<T extends IPersistent<PK>, PK extends Serializable>
 {
-	/**
-	 * @return First matching object
-	 */
-	public abstract T firstMatch();
+    /**
+     * @return Number of objects matching this query
+     */
+    public abstract long countMatches();
 
-	/**
-	 * @param clauses
-	 *            Clauses to use in query
-	 * @return All matching objects
-	 */
-	public abstract List<T>  matches();
-	
-	/**
-	 * @return Number of objects matching this query
-	 */
-	public abstract long countMatches();
+    /**
+     * Delete all objects matching this query
+     */
+    public abstract void delete();
 
-	/**
-	 * Delete all objects matching this query
-	 */
-	public abstract void delete();
+    /**
+     * @return First matching object
+     */
+    public abstract T firstMatch();
+
+    /**
+     * @param clauses
+     *            Clauses to use in query
+     * @return All matching objects
+     */
+    public abstract Iterator<T> matches();
 }

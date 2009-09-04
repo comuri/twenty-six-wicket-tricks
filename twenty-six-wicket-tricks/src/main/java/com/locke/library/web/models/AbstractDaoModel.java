beba7c416c6321aee083266782f435df751bf3dc
jonathan.locke <jonathan.locke@bcb4f866-e984-11dd-bce9-bf72c0239d3a>
2009-09-04 21:48:30 +0000

    removed spring

--- twenty-six-wicket-tricks/src/main/java/com/locke/library/web/models/AbstractDaoModel.java
+++ twenty-six-wicket-tricks/src/main/java/com/locke/library/web/models/AbstractDaoModel.java
@@ -18,7 +18,6 @@ package com.locke.library.web.models;
 
 import java.io.Serializable;
 
-import org.apache.wicket.injection.web.InjectorHolder;
 import org.apache.wicket.model.IModel;
 
 import com.locke.library.persistence.IPersistent;
@@ -31,136 +30,134 @@ import com.locke.library.persistence.dao.IDao;
  * not.
  * 
  * @author Jonathan Locke
- * 
  * @param <T>
  */
 public abstract class AbstractDaoModel<T extends IPersistent<PK>, PK extends Serializable>
-		implements IModel<T>
+                                                                                           implements
+                                                                                           IModel<T>
 {
 
-	private static final long serialVersionUID = -5196138177877713403L;
+    private static final long serialVersionUID = -5196138177877713403L;
 
-	/**
-	 * Id of persistent object if it's been persisted
-	 */
-	private PK id;
+    /**
+     * Id of persistent object if it's been persisted
+     */
+    private PK id;
 
-	/**
-	 * Reference to persistent object (if loaded during this request cycle and
-	 * not yet detached OR if not yet persisted and id is null)
-	 */
-	private T object;
+    /**
+     * Reference to persistent object (if loaded during this request cycle and
+     * not yet detached OR if not yet persisted and id is null)
+     */
+    private T object;
 
-	/**
-	 * @param id
-	 *            Object id
-	 */
-	public AbstractDaoModel(PK id)
-	{
-		this.id = id;
-		InjectorHolder.getInjector().inject(this);
-	}
+    /**
+     * @param model
+     *            Model to copy
+     */
+    public AbstractDaoModel(final IModel<T> model)
+    {
+        // NOTE: We cannot use AbstractDaoModel(T) here because the model object
+        // may not have been fully navigated yet if the DAO is lazy-loading
+        // fields (which might leave some fields null)
+        this(model.getObject().getPrimaryKey());
+    }
 
-	/**
-	 * @param object
-	 *            The object
-	 */
-	public AbstractDaoModel(T object)
-	{
-		this.id = object.getPrimaryKey();
-		this.object = object;
-		InjectorHolder.getInjector().inject(this);
-	}
+    /**
+     * @param id
+     *            Object id
+     */
+    public AbstractDaoModel(final PK id)
+    {
+        this.id = id;
+    }
 
-	/**
-	 * @param model
-	 *            Model to copy
-	 */
-	public AbstractDaoModel(IModel<T> model)
-	{
-		// NOTE: We cannot use AbstractDaoModel(T) here because the model object
-		// may not have been fully navigated yet if the DAO is lazy-loading
-		// fields (which might leave some fields null)
-		this(model.getObject().getPrimaryKey());
-	}
+    /**
+     * @param object
+     *            The object
+     */
+    public AbstractDaoModel(final T object)
+    {
+        this.id = object.getPrimaryKey();
+        this.object = object;
+    }
 
-	/**
-	 * Delete object using DAO
-	 */
-	public void delete()
-	{
-		getDao().delete(getObject());
-	}
+    /**
+     * Delete object using DAO
+     */
+    public void delete()
+    {
+        getDao().delete(getObject());
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void detach()
-	{
+    /**
+     * {@inheritDoc}
+     */
+    public void detach()
+    {
 
-		// If there is an entity
-		if (object != null)
-		{
+        // If there is an entity
+        if (this.object != null)
+        {
 
-			// and it has a persistent id assigned
-			if (object.getPrimaryKey() != null)
-			{
+            // and it has a persistent id assigned
+            if (this.object.getPrimaryKey() != null)
+            {
 
-				// save the id and null out the entity
-				id = object.getPrimaryKey();
-				object = null;
-			}
-		}
-	}
+                // save the id and null out the entity
+                this.id = this.object.getPrimaryKey();
+                this.object = null;
+            }
+        }
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public T getObject()
-	{
+    /**
+     * {@inheritDoc}
+     */
+    public T getObject()
+    {
 
-		// If the entity isn't loaded
-		if (object == null)
-		{
+        // If the entity isn't loaded
+        if (this.object == null)
+        {
 
-			// but there is an id
-			if (id != null)
-			{
+            // but there is an id
+            if (this.id != null)
+            {
 
-				// ask DAO to load it
-				object = getDao().read(id);
-			}
-		}
-		return object;
-	}
+                // ask DAO to load it
+                this.object = getDao().read(this.id);
+            }
+        }
+        return this.object;
+    }
 
-	/**
-	 * Create or Update this persistent model
-	 */
-	public void save()
-	{
-		T object = getObject();
-		if (object.getPrimaryKey() == null)
-		{
-			getDao().create(object);
-		}
-		else
-		{
-			getDao().update(object);
-		}
-	}
+    /**
+     * Create or Update this persistent model
+     */
+    public void save()
+    {
+        final T object = getObject();
+        if (object.getPrimaryKey() == null)
+        {
+            getDao().create(object);
+        }
+        else
+        {
+            getDao().update(object);
+        }
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void setObject(T object)
-	{
-		throw new UnsupportedOperationException(getClass()
-				+ " does not support setObject(T object)");
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public void setObject(final T object)
+    {
+        throw new UnsupportedOperationException(getClass()
+                                                + " does not support setObject(T object)");
+    }
 
-	/**
-	 * @return The DAO for this model
-	 */
-	protected abstract IDao<T, PK> getDao();
+    /**
+     * @return The DAO for this model
+     */
+    protected abstract IDao<T, PK> getDao();
 }
